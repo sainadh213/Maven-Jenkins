2b2a54de5e6898aced7bf4f68bcc3fdc0480bae8
sainadh213 <sainadh213@gmail.com>
2022-05-10 15:21:35 +0530

    New Project

---
+++ src/test/java/MavenExample/CreateUserTest.java
@@ -0,0 +1,12 @@
+package MavenExample;
+
+import org.testng.annotations.Test;
+
+public class CreateUserTest {
+	@Test
+	public void createUserTest()
+	{
+		System.out.println("Create User");
+		
+	}
+}
---
+++ src/test/java/MavenExample/ReadingDataFromCommandLineTest.java
@@ -0,0 +1,21 @@
+package MavenExample;
+
+import org.testng.annotations.Test;
+
+public class ReadingDataFromCommandLineTest {
+	@Test(groups="smoke")
+	public void readingDataFromCommandLineTest()
+	{
+		String url = System.getProperty("url");
+		String un = System.getProperty("username");
+		String pwd = System.getProperty("password");
+		System.out.println(url);
+		System.out.println(un);
+		System.out.println(pwd);
+	}
+	@Test(groups="regression")
+  public void regressionTest() {
+	  System.out.println("This is Regression TestCase");
+	
+}
+}
